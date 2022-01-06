7e91540c85179daa33c582c934a9d4ce7d314869
lbqds <lbqds@outlook.com>
2022-01-06 17:53:51 +0800

    Check block chain index when handle share

--- test/jobManagerTest.js
+++ test/jobManagerTest.js
@@ -10,10 +10,10 @@ describe('test job manager', function(){
         toGroup: 2,
         headerBlob: Buffer.from('0007790e4ec67704f105b406379c6640a1edd9f4f55d07628fd555d72da436dccd50000000003309007511dbb1976d272fdaec0a5ead079da6e422c3c16faa593a3abddd3b822d4602064477d36478ce5a7370e0220e5daadfd9c4984f140a41537f000000005276b90956fa5c3d5b5cdb4235de493e239bd12523266099f0ae9834fd481a7288ec588c73287d61dc9b7ed326e1bc8980bffa383698fe780c1d00153b2890a5ea1c798f8e552c79e44d31fe1d02e2fbef8763369a774502229dd01600000000f07f98e3ec9b7d634293748bac9720310d681dd5a3e87f2f330d61d7c2f0ce1adccebc35c08b472d5d7f1a008009c8291382b839aa8493b57e58cbb400b9146184a084990fe481afb4fea6f0cdd5b372bce59d6be4b7c3b1a1ac72260000017ddbd15f591cffffff', 'hex'),
         txsBlob: Buffer.from('01000a0080004e20bb9aca000001c4261832f67ec288960022daf915864428873f422f045031a889ba81ecbd50db6e967c498b9d8af431e30000017ddbd33419000a01020000017ddbd15f590100000000', 'hex'),
-        targetBlob: Buffer.from('00ffffff00000000000000000000000000000000000000000000000000', 'hex')
+        targetBlob: Buffer.from('00ffffff00000000000000000000000000000000000000000000000000000000', 'hex')
     };
 
-    var nonce = 'ce5666fb5d8b65abfddd92bcb60f1f8852745ceb719454f8';
+    var nonce = 'f8c8741232a4ebb0aad38ffb8e829b9bf5b00770f1ac31dc';
     var address = '1AqVGKeHWoLJiVU7heL8EvwQN2hk5bMtvP3PsH57qWayr';
     var invalidAddress = '114E4tiwXSyfvCqLnARL21Ac2pVS6GvPomw5y6HsLMwuyR';
     var defaultHost = '127.0.0.1';
@@ -106,12 +106,23 @@ describe('test job manager', function(){
     it('should process share failed if difficulty is low', function(){
         var jobManager = new JobManager();
         jobManager.addJob(job, Date.now());
-        var lowDiffNonce = '000000000000000000000000000000000000000000000000';
+        var lowDiffNonce = '301d0b1f7e61e1d532d37df520f1acc92cfccc48de323e1c';
 
         var params = {jobId: job.jobId, fromGroup: job.fromGroup, toGroup: job.toGroup, nonce: lowDiffNonce, worker: address};
         var result = processShare(jobManager, params, 2, 2);
         var [errCode, _] = result.error;
         expect(errCode).equal(25);
+    })
+
+    it('should process share failed if chain index unmatched', function(){
+        var jobManager = new JobManager();
+        jobManager.addJob(job, Date.now());
+        var invalidNonce = 'b6414be3a40e1a2852b3171e4462846ea48c777104b03e5e';
+
+        var params = {jobId: job.jobId, fromGroup: job.fromGroup, toGroup: job.toGroup, nonce: invalidNonce, worker: address};
+        var result = processShare(jobManager, params, 2, 2);
+        var [errCode, _] = result.error;
+        expect(errCode).equal(27);
     })
 
     it('should accept share if difficulty larger than current difficulty', function(){
